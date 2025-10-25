40caad7dde9c432cdce69e73beb6896958bdc283
lonelystar <65070531+xzq060312@users.noreply.github.com>
2025-10-25 18:25:54 +0800

    Delete robocon_zero directory

--- robocon_zero/kf_decision/__init__.py
+++
@@ -1,7 +0,0 @@
-# kf_decision/__init__.py
-# 先扫 impl 包，让装饰器跑到
-from . import simple_policy, r1_policy   # noqa: F401
-# 再导出工具函数
-from .registry import get_policy, list_policies
-
-__all__ = ["get_policy", "list_policies"]
