d473b97cb420700dbb9af7b0a757679ba6b7138a
lonelystar <Lumivers@outlook.com>
2026-05-15 23:31:19 +0800

    添加 robot_serial 包及相关消息类型，更新 r2_decision 和 r2_fake_upper_body 节点以使用新消息

--- src/r2_fake_upper_body/src/fake_upper_body.node.cpp
+++ src/r2_fake_upper_body/src/fake_upper_body.node.cpp
@@ -1,5 +1,5 @@
 #include <rclcpp/rclcpp.hpp>
-#include "r2_interfaces/msg/arm_command.hpp"
+#include "robot_serial/msg/juece.hpp"
 #include "r2_interfaces/msg/arm_ack.hpp"
 #include "r2_interfaces/msg/arm_done.hpp"
 
@@ -10,17 +10,17 @@ class FakeUpperBodyNode : public rclcpp::Node
 public:
     FakeUpperBodyNode() : Node("juece")
     {
-        sub_ = create_subscription<r2_interfaces::msg::ArmCommand>(
-            "/r2/upper_body/command",
+        sub_ = create_subscription<robot_serial::msg::Juece>(
+            "/juece",
             10,
             std::bind(&FakeUpperBodyNode::onCommand, this, _1));
 
         ack_pub_ = create_publisher<r2_interfaces::msg::ArmAck>(
-            "/r2/upper_body/ack",
+            "/juece_ack",
             10);
 
         pub_ = create_publisher<r2_interfaces::msg::ArmDone>(
-            "/r2/upper_body/done",
+            "/juece_done",
             10);
 
         RCLCPP_INFO(this->get_logger(), "Fake upper body node started as juece");
@@ -44,11 +44,11 @@ private:
 
         switch (command)
         {
-        case r2_interfaces::msg::ArmCommand::UP_STAIRS:
+        case 1:  // UP_STAIRS
             RCLCPP_INFO(this->get_logger(), "Fake UP_STAIRS");
             break;
 
-        case r2_interfaces::msg::ArmCommand::DOWN_STAIRS:
+        case 2:  // DOWN_STAIRS
             RCLCPP_INFO(this->get_logger(), "Fake DOWN_STAIRS");
             break;
 
@@ -58,7 +58,7 @@ private:
 
         pub_->publish(done_msg);
         command_in_progress_ = false;
-        active_command_ = r2_interfaces::msg::ArmCommand::IDLE;
+        active_command_ = 0;
 
         if (finish_timer_)
         {
@@ -67,53 +67,53 @@ private:
         }
     }
 
-    void onCommand(const r2_interfaces::msg::ArmCommand::SharedPtr msg)
+    void onCommand(const robot_serial::msg::Juece::SharedPtr msg)
     {
         RCLCPP_INFO(
             this->get_logger(),
             "Received arm command: %d",
-            msg->command);
+            msg->status_bit);
 
-        publishAck(msg->command);
+        publishAck(msg->status_bit);
 
         if (command_in_progress_)
         {
-            if (msg->command == active_command_)
+            if (msg->status_bit == active_command_)
             {
                 RCLCPP_INFO(
                     this->get_logger(),
                     "Duplicate command while running, ACK only: %d",
-                    msg->command);
+                    msg->status_bit);
             }
             else
             {
                 RCLCPP_WARN(
                     this->get_logger(),
                     "New command %d received while command %d still running, ignored",
-                    msg->command,
+                    msg->status_bit,
                     active_command_);
             }
             return;
         }
 
         command_in_progress_ = true;
-        active_command_ = msg->command;
+        active_command_ = msg->status_bit;
 
         finish_timer_ = create_wall_timer(
             std::chrono::milliseconds(500),
-            [this, cmd = msg->command]()
+            [this, cmd = msg->status_bit]()
             {
                 finishCommand(cmd);
             });
     }
 
-    rclcpp::Subscription<r2_interfaces::msg::ArmCommand>::SharedPtr sub_;
+    rclcpp::Subscription<robot_serial::msg::Juece>::SharedPtr sub_;
     rclcpp::Publisher<r2_interfaces::msg::ArmAck>::SharedPtr ack_pub_;
     rclcpp::Publisher<r2_interfaces::msg::ArmDone>::SharedPtr pub_;
     rclcpp::TimerBase::SharedPtr finish_timer_;
 
     bool command_in_progress_{false};
-    uint8_t active_command_{r2_interfaces::msg::ArmCommand::IDLE};
+    uint8_t active_command_{0};
 };
 
 int main(int argc, char **argv)
